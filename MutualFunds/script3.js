8d869ed8d3d9181dcb5916bfb4dcaded10d82ec5
NP <thenotionpod@gmail.com>
2024-09-04 18:55:00 +0530

    MF Code with Slider 5

--- MutualFunds/script3.js
+++ MutualFunds/script3.js
@@ -3,320 +3,380 @@ const cardsController = document.querySelector(".card-carousel + .card-controlle
 
 
 class DraggingEvent {
-  constructor(target = undefined) {
-    this.target = target;
-  }
-  
-  event(callback) {
-    let handler;
-    
-    this.target.addEventListener("mousedown", e => {
-      e.preventDefault()
-      
-      handler = callback(e)
-      
-      window.addEventListener("mousemove", handler)
-      
-      document.addEventListener("mouseleave", clearDraggingEvent)
-      
-      window.addEventListener("mouseup", clearDraggingEvent)
-      
-      function clearDraggingEvent() {
-        window.removeEventListener("mousemove", handler)
-        window.removeEventListener("mouseup", clearDraggingEvent)
-      
-        document.removeEventListener("mouseleave", clearDraggingEvent)
-        
-        handler(null)
-      }
-    })
-    
-    this.target.addEventListener("touchstart", e => {
-      handler = callback(e)
-      
-      window.addEventListener("touchmove", handler)
-      
-      window.addEventListener("touchend", clearDraggingEvent)
-      
-      document.body.addEventListener("mouseleave", clearDraggingEvent)
-      
-      function clearDraggingEvent() {
-        window.removeEventListener("touchmove", handler)
-        window.removeEventListener("touchend", clearDraggingEvent)
-        
-        handler(null)
-      }
-    })
-  }
-  
-  // Get the distance that the user has dragged
-  getDistance(callback) {
-    function distanceInit(e1) {
-      let startingX, startingY;
-      
-      if ("touches" in e1) {
-        startingX = e1.touches[0].clientX
-        startingY = e1.touches[0].clientY
-      } else {
-        startingX = e1.clientX
-        startingY = e1.clientY
-      }
-      
-
-      return function(e2) {
-        if (e2 === null) {
-          return callback(null)
-        } else {
-          
-          if ("touches" in e2) {
-            return callback({
-              x: e2.touches[0].clientX - startingX,
-              y: e2.touches[0].clientY - startingY
-            })
-          } else {
-            return callback({
-              x: e2.clientX - startingX,
-              y: e2.clientY - startingY
-            })
-          }
-        }
-      }
+    constructor(target = undefined) {
+        this.target = target;
     }
-    
-    this.event(distanceInit)
-  }
+
+    event(callback) {
+        let handler;
+
+        this.target.addEventListener("mousedown", e => {
+            e.preventDefault()
+
+            handler = callback(e)
+
+            window.addEventListener("mousemove", handler)
+
+            document.addEventListener("mouseleave", clearDraggingEvent)
+
+            window.addEventListener("mouseup", clearDraggingEvent)
+
+            function clearDraggingEvent() {
+                window.removeEventListener("mousemove", handler)
+                window.removeEventListener("mouseup", clearDraggingEvent)
+
+                document.removeEventListener("mouseleave", clearDraggingEvent)
+
+                handler(null)
+            }
+        })
+
+        this.target.addEventListener("touchstart", e => {
+            handler = callback(e)
+
+            window.addEventListener("touchmove", handler)
+
+            window.addEventListener("touchend", clearDraggingEvent)
+
+            document.body.addEventListener("mouseleave", clearDraggingEvent)
+
+            function clearDraggingEvent() {
+                window.removeEventListener("touchmove", handler)
+                window.removeEventListener("touchend", clearDraggingEvent)
+
+                handler(null)
+            }
+        })
+    }
+
+    // Get the distance that the user has dragged
+    getDistance(callback) {
+        function distanceInit(e1) {
+            let startingX, startingY;
+
+            if ("touches" in e1) {
+                startingX = e1.touches[0].clientX
+                startingY = e1.touches[0].clientY
+            } else {
+                startingX = e1.clientX
+                startingY = e1.clientY
+            }
+
+
+            return function (e2) {
+                if (e2 === null) {
+                    return callback(null)
+                } else {
+
+                    if ("touches" in e2) {
+                        return callback({
+                            x: e2.touches[0].clientX - startingX,
+                            y: e2.touches[0].clientY - startingY
+                        })
+                    } else {
+                        return callback({
+                            x: e2.clientX - startingX,
+                            y: e2.clientY - startingY
+                        })
+                    }
+                }
+            }
+        }
+
+        this.event(distanceInit)
+    }
 }
 
 
 class CardCarousel extends DraggingEvent {
-  constructor(container, controller = undefined) {
-    super(container)
-    
-    // DOM elements
-    this.container = container
-    this.controllerElement = controller
-    this.cards = container.querySelectorAll(".card")
-    
-    // Carousel data
-    this.centerIndex = (this.cards.length - 1) / 2;
-    this.cardWidth = this.cards[0].offsetWidth / this.container.offsetWidth * 100
-    this.xScale = {};
-    
-    // Resizing
-    window.addEventListener("resize", this.updateCardWidth.bind(this))
-    
-    if (this.controllerElement) {
-      this.controllerElement.addEventListener("keydown", this.controller.bind(this))      
-    }
+    constructor(container, controller = undefined) {
+        super(container)
 
-    
-    // Initializers
-    this.build()
-    
-    // Bind dragging event
-    super.getDistance(this.moveCards.bind(this))
-  }
-  
-  updateCardWidth() {
-    this.cardWidth = this.cards[0].offsetWidth / this.container.offsetWidth * 100
-    
-    this.build()
-  }
-  
-  build(fix = 0) {
-    for (let i = 0; i < this.cards.length; i++) {
-      const x = i - this.centerIndex;
-      const scale = this.calcScale(x)
-      const scale2 = this.calcScale2(x)
-      const zIndex = -(Math.abs(i - this.centerIndex))
-      
-      const leftPos = this.calcPos(x, scale2)
-     
-      
-      this.xScale[x] = this.cards[i]
-      
-      this.updateCards(this.cards[i], {
-        x: x,
-        scale: scale,
-        leftPos: leftPos,
-        zIndex: zIndex
-      })
-    }
-  }
-  
-  
-  controller(e) {
-    const temp = {...this.xScale};
-      
-      if (e.keyCode === 39) {
-        // Left arrow
-        for (let x in this.xScale) {
-          const newX = (parseInt(x) - 1 < -this.centerIndex) ? this.centerIndex : parseInt(x) - 1;
+        // DOM elements
+        this.container = container
+        this.controllerElement = controller
+        this.cards = container.querySelectorAll(".card")
 
-          temp[newX] = this.xScale[x]
+        const jsonPath = 'mf.json';
+
+        fetch(jsonPath)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                // Ensure data is an array
+                if (!Array.isArray(data)) {
+                    throw new Error('mf.json does not contain an array');
+                }
+
+                // Loop through each value and make API calls
+                data.forEach((value, index) => {
+                    // Replace with your actual third-party API URL and parameters
+                    const apiUrl = `https://api.mfapi.in/mf/${value}/latest`;
+
+                    fetch(apiUrl)
+                        .then(apiResponse => {
+                            if (!apiResponse.ok) {
+                                throw new Error(`API error for ${value}: ${apiResponse.status}`);
+                            }
+                            return apiResponse.json();
+                        })
+                        .then(apiData => {
+                            // Handle the API response data
+                            console.log(`Data for ${value}:`, apiData);
+                            // You can update the DOM or perform other actions here
+                            displayData(index + 1,value, apiData);
+                        })
+                        .catch(apiError => {
+                            console.error(apiError);
+                        });
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching mf.json:', error);
+            });
+
+
+        // Carousel data
+        this.centerIndex = (this.cards.length - 1) / 2;
+        this.cardWidth = this.cards[0].offsetWidth / this.container.offsetWidth * 100
+        this.xScale = {};
+
+        // Resizing
+        window.addEventListener("resize", this.updateCardWidth.bind(this))
+
+        if (this.controllerElement) {
+            this.controllerElement.addEventListener("keydown", this.controller.bind(this))
         }
-      }
-      
-      if (e.keyCode == 37) {
-        // Right arrow
-        for (let x in this.xScale) {
-          const newX = (parseInt(x) + 1 > this.centerIndex) ? -this.centerIndex : parseInt(x) + 1;
 
-          temp[newX] = this.xScale[x]
+
+        // Initializers
+        this.build()
+
+        // Bind dragging event
+        super.getDistance(this.moveCards.bind(this))
+    }
+
+    updateCardWidth() {
+        this.cardWidth = this.cards[0].offsetWidth / this.container.offsetWidth * 100
+
+        this.build()
+    }
+
+    build(fix = 0) {
+        for (let i = 0; i < this.cards.length; i++) {
+            const x = i - this.centerIndex;
+            const scale = this.calcScale(x)
+            const scale2 = this.calcScale2(x)
+            const zIndex = -(Math.abs(i - this.centerIndex))
+
+            const leftPos = this.calcPos(x, scale2)
+
+
+            this.xScale[x] = this.cards[i]
+
+            this.updateCards(this.cards[i], {
+                x: x,
+                scale: scale,
+                leftPos: leftPos,
+                zIndex: zIndex
+            })
         }
-      }
-      
-      this.xScale = temp;
-      
-      for (let x in temp) {
-        const scale = this.calcScale(x),
-              scale2 = this.calcScale2(x),
-              leftPos = this.calcPos(x, scale2),
-              zIndex = -Math.abs(x)
+    }
 
-        this.updateCards(this.xScale[x], {
-          x: x,
-          scale: scale,
-          leftPos: leftPos,
-          zIndex: zIndex
-        })
-      }
-  }
-  
-  calcPos(x, scale) {
-    let formula;
-    
-    if (x < 0) {
-      formula = (scale * 100 - this.cardWidth) / 2
-      
-      return formula
 
-    } else if (x > 0) {
-      formula = 100 - (scale * 100 + this.cardWidth) / 2
-      
-      return formula
-    } else {
-      formula = 100 - (scale * 100 + this.cardWidth) / 2
-      
-      return formula
-    }
-  }
-  
-  updateCards(card, data) {
-    if (data.x || data.x == 0) {
-      card.setAttribute("data-x", data.x)
-    }
-    
-    if (data.scale || data.scale == 0) {
-      card.style.transform = `scale(${data.scale})`
+    controller(e) {
+        const temp = { ...this.xScale };
 
-      if (data.scale == 0) {
-        card.style.opacity = data.scale
-      } else {
-        card.style.opacity = 1;
-      }
-    }
-   
-    if (data.leftPos) {
-      card.style.left = `${data.leftPos}%`        
-    }
-    
-    if (data.zIndex || data.zIndex == 0) {
-      if (data.zIndex == 0) {
-        card.classList.add("highlight")
-      } else {
-        card.classList.remove("highlight")
-      }
-      
-      card.style.zIndex = data.zIndex  
-    }
-  }
-  
-  calcScale2(x) {
-    let formula;
-   
-    if (x <= 0) {
-      formula = 1 - -1 / 5 * x
-      
-      return formula
-    } else if (x > 0) {
-      formula = 1 - 1 / 5 * x
-      
-      return formula
-    }
-  }
-  
-  calcScale(x) {
-    const formula = 1 - 1 / 5 * Math.pow(x, 2)
-    
-    if (formula <= 0) {
-      return 0 
-    } else {
-      return formula      
-    }
-  }
-  
-  checkOrdering(card, x, xDist) {    
-    const original = parseInt(card.dataset.x)
-    const rounded = Math.round(xDist)
-    let newX = x
-    
-    if (x !== x + rounded) {
-      if (x + rounded > original) {
-        if (x + rounded > this.centerIndex) {
-          
-          newX = ((x + rounded - 1) - this.centerIndex) - rounded + -this.centerIndex
+        if (e.keyCode === 39) {
+            // Left arrow
+            for (let x in this.xScale) {
+                const newX = (parseInt(x) - 1 < -this.centerIndex) ? this.centerIndex : parseInt(x) - 1;
+
+                temp[newX] = this.xScale[x]
+            }
         }
-      } else if (x + rounded < original) {
-        if (x + rounded < -this.centerIndex) {
-          
-          newX = ((x + rounded + 1) + this.centerIndex) - rounded + this.centerIndex
+
+        if (e.keyCode == 37) {
+            // Right arrow
+            for (let x in this.xScale) {
+                const newX = (parseInt(x) + 1 > this.centerIndex) ? -this.centerIndex : parseInt(x) + 1;
+
+                temp[newX] = this.xScale[x]
+            }
         }
-      }
-      
-      this.xScale[newX + rounded] = card;
-    }
-    
-    const temp = -Math.abs(newX + rounded)
-    
-    this.updateCards(card, {zIndex: temp})
 
-    return newX;
-  }
-  
-  moveCards(data) {
-    let xDist;
-    
-    if (data != null) {
-      this.container.classList.remove("smooth-return")
-      xDist = data.x / 250;
-    } else {
+        this.xScale = temp;
 
-      
-      this.container.classList.add("smooth-return")
-      xDist = 0;
+        for (let x in temp) {
+            const scale = this.calcScale(x),
+                scale2 = this.calcScale2(x),
+                leftPos = this.calcPos(x, scale2),
+                zIndex = -Math.abs(x)
 
-      for (let x in this.xScale) {
-        this.updateCards(this.xScale[x], {
-          x: x,
-          zIndex: Math.abs(Math.abs(x) - this.centerIndex)
-        })
-      }
+            this.updateCards(this.xScale[x], {
+                x: x,
+                scale: scale,
+                leftPos: leftPos,
+                zIndex: zIndex
+            })
+        }
     }
 
-    for (let i = 0; i < this.cards.length; i++) {
-      const x = this.checkOrdering(this.cards[i], parseInt(this.cards[i].dataset.x), xDist),
-            scale = this.calcScale(x + xDist),
-            scale2 = this.calcScale2(x + xDist),
-            leftPos = this.calcPos(x + xDist, scale2)
-      
-      
-      this.updateCards(this.cards[i], {
-        scale: scale,
-        leftPos: leftPos
-      })
+    calcPos(x, scale) {
+        let formula;
+
+        if (x < 0) {
+            formula = (scale * 100 - this.cardWidth) / 2
+
+            return formula
+
+        } else if (x > 0) {
+            formula = 100 - (scale * 100 + this.cardWidth) / 2
+
+            return formula
+        } else {
+            formula = 100 - (scale * 100 + this.cardWidth) / 2
+
+            return formula
+        }
     }
-  }
+
+    updateCards(card, data) {
+        if (data.x || data.x == 0) {
+            card.setAttribute("data-x", data.x)
+        }
+
+        if (data.scale || data.scale == 0) {
+            card.style.transform = `scale(${data.scale})`
+
+            if (data.scale == 0) {
+                card.style.opacity = data.scale
+            } else {
+                card.style.opacity = 1;
+            }
+        }
+
+        if (data.leftPos) {
+            card.style.left = `${data.leftPos}%`
+        }
+
+        if (data.zIndex || data.zIndex == 0) {
+            if (data.zIndex == 0) {
+                card.classList.add("highlight")
+            } else {
+                card.classList.remove("highlight")
+            }
+
+            card.style.zIndex = data.zIndex
+        }
+    }
+
+    calcScale2(x) {
+        let formula;
+
+        if (x <= 0) {
+            formula = 1 - -1 / 5 * x
+
+            return formula
+        } else if (x > 0) {
+            formula = 1 - 1 / 5 * x
+
+            return formula
+        }
+    }
+
+    calcScale(x) {
+        const formula = 1 - 1 / 5 * Math.pow(x, 2)
+
+        if (formula <= 0) {
+            return 0
+        } else {
+            return formula
+        }
+    }
+
+    checkOrdering(card, x, xDist) {
+        const original = parseInt(card.dataset.x)
+        const rounded = Math.round(xDist)
+        let newX = x
+
+        if (x !== x + rounded) {
+            if (x + rounded > original) {
+                if (x + rounded > this.centerIndex) {
+
+                    newX = ((x + rounded - 1) - this.centerIndex) - rounded + -this.centerIndex
+                }
+            } else if (x + rounded < original) {
+                if (x + rounded < -this.centerIndex) {
+
+                    newX = ((x + rounded + 1) + this.centerIndex) - rounded + this.centerIndex
+                }
+            }
+
+            this.xScale[newX + rounded] = card;
+        }
+
+        const temp = -Math.abs(newX + rounded)
+
+        this.updateCards(card, { zIndex: temp })
+
+        return newX;
+    }
+
+    moveCards(data) {
+        let xDist;
+
+        if (data != null) {
+            this.container.classList.remove("smooth-return")
+            xDist = data.x / 250;
+        } else {
+
+
+            this.container.classList.add("smooth-return")
+            xDist = 0;
+
+            for (let x in this.xScale) {
+                this.updateCards(this.xScale[x], {
+                    x: x,
+                    zIndex: Math.abs(Math.abs(x) - this.centerIndex)
+                })
+            }
+        }
+
+        for (let i = 0; i < this.cards.length; i++) {
+            const x = this.checkOrdering(this.cards[i], parseInt(this.cards[i].dataset.x), xDist),
+                scale = this.calcScale(x + xDist),
+                scale2 = this.calcScale2(x + xDist),
+                leftPos = this.calcPos(x + xDist, scale2)
+
+
+            this.updateCards(this.cards[i], {
+                scale: scale,
+                leftPos: leftPos
+            })
+        }
+    }
 }
 
+function displayData(count,value, data) {
+    const container = document.getElementById('data-container');
+
+    const item = document.createElement('div');
+    item.classList.add('card');
+    item.setAttribute('id',count);
+
+    const title = document.createElement('p');
+    title.textContent = `Data for ${value}`;
+
+    //const content = document.createElement('pre');
+    //content.textContent = JSON.stringify(data, null, 2);
+
+    item.appendChild(title);
+    //item.appendChild(content);
+    container.appendChild(item);
+}
 const carousel = new CardCarousel(cardsContainer)
